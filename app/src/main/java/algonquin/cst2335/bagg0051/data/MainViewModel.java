bdd8e1fa2526c34073c8face99247ad5f4bd634d
Collin Baggio <bagg0051@algonquinlive.com>
2023-05-26 19:46:36 -0400

    Finished week 2

--- app/src/main/java/algonquin/cst2335/bagg0051/data/MainViewModel.java
+++ app/src/main/java/algonquin/cst2335/bagg0051/data/MainViewModel.java
@@ -1,8 +1,9 @@
 package algonquin.cst2335.bagg0051.data;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
-
     public MutableLiveData<String> editString = new MutableLiveData<>();
+    public MutableLiveData<Boolean> isSelected = new MutableLiveData<>();
 }
--- app/src/main/java/algonquin/cst2335/bagg0051/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/bagg0051/ui/MainActivity.java
@@ -1,14 +1,12 @@
 package algonquin.cst2335.bagg0051.ui;
 
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.os.Bundle;
-
+import algonquin.cst2335.bagg0051.R;
 import algonquin.cst2335.bagg0051.data.MainViewModel;
 import algonquin.cst2335.bagg0051.databinding.ActivityMainBinding;
 
@@ -26,13 +24,43 @@ public class MainActivity extends AppCompatActivity {
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(variableBinding.getRoot());
 
-        variableBinding.textview.setText("Your edit has: " + model.editString);
-        variableBinding.mybutton.setOnClickListener(click ->
-        {
-            model.editString.observe(this, s -> {
-               variableBinding.textview.setText("Your edit text has "+ s);
-            });
+        variableBinding.textview.setText("Your edit has: " + model.editString.getValue());
+        variableBinding.mybutton.setOnClickListener(click -> {
+            model.editString.setValue(variableBinding.myedittext.getText().toString());
         });
-        }
 
+        // Register MainActivity as an observer of the MutableLiveData<Boolean> variable
+        model.isSelected.observe(this, selected -> {
+            // Update the CompoundButton objects (CheckBox, Switch, RadioButton)
+            variableBinding.checkbox.setChecked(selected);
+            variableBinding.myswitch.setChecked(selected);
+            variableBinding.radiobutton.setChecked(selected);
+
+            //Show a toast message with the updated value
+            String toastMessage = "The value is now: " + selected;
+            Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
+        });
+
+        // Set onCheckedChangedListener for each CompoundButton
+        variableBinding.checkbox.setOnCheckedChangeListener((compoundButton, isChecked) -> {
+            model.isSelected.setValue(isChecked);
+        });
+
+        variableBinding.myswitch.setOnCheckedChangeListener((compoundButton, isChecked) -> {
+            model.isSelected.setValue(isChecked);
+        });
+
+        variableBinding.radiobutton.setOnCheckedChangeListener((compoundButton, isChecked) -> {
+            model.isSelected.setValue(isChecked);
+        });
+
+        variableBinding.imageview.setImageResource(R.drawable.logo_algonquin);
+
+        variableBinding.myimagebutton.setOnClickListener(view -> {
+            int width = view.getWidth();
+            int height = view.getHeight();
+            String toastMessage = "The width = " + width + " and height = " + height;
+            Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
+        });
     }
+}
